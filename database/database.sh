93478fe459b40aeb8fcfd0fb1a89eb2b8ff23739
Weverton Campos <79879949+wevertoncamposdev@users.noreply.github.com>
2023-05-26 20:21:36 -0300

    update

--- database/database.sh
+++ database/database.sh
@@ -4,7 +4,7 @@
 docker pull postgres
 
 # Criar o contêiner do PostgreSQL
-docker run --name database -e POSTGRES_PASSWORD=e8xf1538fet -p 5432:5432 -d postgres
+docker run --name database -e POSTGRES_PASSWORD=e8xf1538fet -p 5432:5432 -d postgres --network=backend
 
 # Conectar ao banco: psql -h localhost -p 5432 -U postgres
 
