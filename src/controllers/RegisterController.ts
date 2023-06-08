9d7bf224e004aeb2f5e5266ccc5fa1e5e0889fad
Weverton Campos <79879949+wevertoncamposdev@users.noreply.github.com>
2023-06-08 09:53:43 -0300

    update

--- src/controllers/RegisterController.ts
+++ src/controllers/RegisterController.ts
@@ -3,14 +3,54 @@ import { LogController } from "./LogController";
 
 export class RegisterController {
 
-    async service(req: Request, res: Response) {
-        
-            //acess service
+    async read(req: Request, res: Response) {
 
-            // Aqui você pode fazer uma requisição para o serviço desejado e retornar a resposta
-            // Exemplo:
-            // axios.get('https://servico.com/api/dados')
-            //   .then(response => res.json(response.data))
-            //   .catch(error => res.status(500).json({ error: 'Internal Server Error' }));
+        //acess service
+
+        // Aqui você pode fazer uma requisição para o serviço desejado e retornar a resposta
+        // Exemplo:
+        // axios.get('https://servico.com/api/dados')
+        //   .then(response => res.json(response.data))
+        //   .catch(error => res.status(500).json({ error: 'Internal Server Error' }));
+        return res.json('Route Register Method GET')
+    }
+
+    async create(req: Request, res: Response) {
+
+        //acess service
+
+        // Aqui você pode fazer uma requisição para o serviço desejado e retornar a resposta
+        // Exemplo:
+        // axios.get('https://servico.com/api/dados')
+        //   .then(response => res.json(response.data))
+        //   .catch(error => res.status(500).json({ error: 'Internal Server Error' }));
+
+        return res.json('Route Register Method POST')
+    }
+
+    async update(req: Request, res: Response) {
+
+        //acess service
+
+        // Aqui você pode fazer uma requisição para o serviço desejado e retornar a resposta
+        // Exemplo:
+        // axios.get('https://servico.com/api/dados')
+        //   .then(response => res.json(response.data))
+        //   .catch(error => res.status(500).json({ error: 'Internal Server Error' }));
+
+        return res.json('Route Register Method PUT')
+    }
+
+    async delete(req: Request, res: Response) {
+
+        //acess service
+
+        // Aqui você pode fazer uma requisição para o serviço desejado e retornar a resposta
+        // Exemplo:
+        // axios.get('https://servico.com/api/dados')
+        //   .then(response => res.json(response.data))
+        //   .catch(error => res.status(500).json({ error: 'Internal Server Error' }));
+
+        return res.json('Route Register Method DELETE')
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
+//Library
 import { Router } from 'express';
-import { RoleController } from './controllers/RoleController';
-import { UserController } from './controllers/UserController';
-import { LogController } from './controllers/LogController';
-import { RegisterController } from './controllers/RegisterController';
+import { Request, Response } from "express";
+
+//Middleware
 import authenticationMiddleware from './middleware/authenticationMiddleware';
 import authorizationMiddleware from './middleware/authorizationMiddleware';
+
+//Controllers
+import { RegisterController } from './controllers/RegisterController';
+import { LogController } from './controllers/LogController';
+
 
 const routes = Router();
 
@@ -13,25 +18,38 @@ routes.get('/', (req, res) => {
 });
 
 //Services
-routes.get('/register', authenticationMiddleware, authorizationMiddleware, new RegisterController().service);
-routes.get('/finance', new RoleController().find);
+routes.route('/register')
+.post(new RegisterController().create)
+.get(new RegisterController().read)
+.put(new RegisterController().update)
+.delete(new RegisterController().delete)
 
-//ROLE
-routes.get('/role', new RoleController().find);
-routes.get('/role/:id', new RoleController().findOne);
-routes.post('/role', new RoleController().create);
-//update
-//delete
-
-//USER
-routes.get('/user', new UserController().find);
-routes.get('/user/:id', new UserController().findOne);
-routes.post('/user', new UserController().create);
-//update
-//delete
-
-//LOG
 routes.get('/logs', new LogController().find);
-routes.get('/logs/:id', new LogController().findOne);
 
 export default routes;
+
+
+/* 
+.post(authenticationMiddleware, authorizationMiddleware, new RegisterController().create);
+.get(authenticationMiddleware, authorizationMiddleware, new RegisterController().read);
+.put(authenticationMiddleware, authorizationMiddleware, new RegisterController().update);
+.delete(authenticationMiddleware, authorizationMiddleware, new RegisterController().delete); 
+*/
+
+
+
+/* import { RoleController } from './controllers/RoleController';
+import { UserController } from './controllers/UserController';
+ */
+
+//ROLE
+/* routes.get('/role', new RoleController().find);
+routes.get('/role/:id', new RoleController().findOne);
+routes.post('/role', new RoleController().create); */
+
+//USER
+/* routes.get('/user', new UserController().find);
+routes.get('/user/:id', new UserController().findOne);
+routes.post('/user', new UserController().create); */
+
+//LOG
