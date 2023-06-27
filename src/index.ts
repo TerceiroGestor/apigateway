b98b0caa3ad12b3a5baa712ea3f143b9e2a48ea9
Weverton Campos <79879949+wevertoncamposdev@users.noreply.github.com>
2023-06-27 20:53:46 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-import 'dotenv/config'
+import 'dotenv/config';
 import { AppDataSource } from './data-source';
 import routes from "./routes";
 
