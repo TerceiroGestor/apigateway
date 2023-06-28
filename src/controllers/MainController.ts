385d797412e4d481a53c401e0e000d8d18d6a661
Weverton Campos <79879949+wevertoncamposdev@users.noreply.github.com>
2023-06-28 07:33:10 -0300

    update maincontroller

--- src/controllers/MainController.ts
+++ src/controllers/MainController.ts
@@ -1,19 +1,9 @@
 import { Request, Response } from "express";
 import { LogController } from "./LogController";
 
-
-
 export class MainController {
 
     async create(req: Request, res: Response) {
-
-        //acess service
-
-        // Aqui você pode fazer uma requisição para o serviço desejado e retornar a resposta
-        // Exemplo:
-        // axios.get('https://servico.com/api/dados')
-        //   .then(response => res.json(response.data))
-        //   .catch(error => res.status(500).json({ error: 'Internal Server Error' }));
 
         return res.json({
             'response': 'API Gateway',
@@ -23,6 +13,7 @@ export class MainController {
             'body': req.body 
         })
     }
+
     async read(req: Request, res: Response) {
         const data = req.header;
         return res.json({
